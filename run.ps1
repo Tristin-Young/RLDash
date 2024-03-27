88250b1643547ec2e59ed16c020f24a9f28cca3a
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2024-03-27 16:42:44 -0400

    Added scripts to make updating/running easier

---
+++ run.ps1
@@ -0,0 +1,9 @@
+# Script to run the Rocket League Overlay
+# Author: Tristin Young
+# Date: 3/27/2024
+
+# 1. Navigate to the RLDash directory
+Set-Location -Path RLDash
+
+# 2. Run the project
+npm start
