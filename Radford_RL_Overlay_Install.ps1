0688dd4b9d8b1f8729e9ec1a86b5a583420e114c
Tristin-Young <86166175+Tristin-Young@users.noreply.github.com>
2024-03-27 14:57:07 -0400

    Radford Overlay Install

---
+++ Radford_RL_Overlay_Install.ps1
@@ -0,0 +1,16 @@
+# Installation script for the Rocket League Overlay
+# This install only pertains to Windows-based systems
+# Author: Tristin Young
+# Date: 3/27/2024
+
+# 1. Git Clone Project
+git clone --branch RadfordFinal --single-branch https://github.com/Tristin-Young/RLDash.git
+
+# 2. Navigate to RLDash directory
+Set-Location -Path RLDash
+
+# 3. Install dependencies
+npm install
+
+# 4. Build the project
+npm run build
--- src/models/MatchEnded/MatchEnded.ts
+++
@@ -1,5 +0,0 @@
-export interface MatchEnded {
-
-    winner_team_num: number;
-    
-}
